031a93d630f71318d7a97cf7dc10045097f5f08c
Joseph Kerkhof <musicaljoeker@gmail.com>
2018-07-02 23:15:37 -0500

    Basic layout of the top menu

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import logo from "./logo.png";
 import "./App.css";
+import Menu from "./Menu.js";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code>
-          and save to reload.
-        </p>
+        <nav className="App-nav">
+          <Menu />
+          <img src={logo} className="App-logo" alt="logo for Joseph Kerkhof" />
+        </nav>
+        <main className="App-main">here is some junk text</main>
       </div>
     );
   }
---
+++ src/Menu.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import "./Menu.css";
+
+class Menu extends Component {
+  render() {
+    return (
+      <div className="Menu-icon">
+        <div className="Menu-icon-line" />
+        <div className="Menu-icon-line" />
+        <div className="Menu-icon-line" />
+      </div>
+    );
+  }
+}
+
+export default Menu;
