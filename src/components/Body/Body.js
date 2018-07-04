5ce845a5a192665a85c7bbaeaea19e02fcb54fe4
Joseph Kerkhof <musicaljoeker@gmail.com>
2018-07-04 09:31:06 -0500

    Nav height and body additions

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -5,7 +5,33 @@ class Body extends Component {
   render() {
     return (
       <div className="body-wrapper">
-        <h1>Hi there</h1>
+        <h1 className="greeting">
+          Hi{" "}
+          <span role="img" aria-label="light skinned waving hand emoji">
+            👋🏻
+          </span>
+          ,
+        </h1>
+        <div className="body-content">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Duis aute irure dolor in
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+            pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+            culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+        </div>
       </div>
     );
   }
