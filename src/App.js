4cd811e2309d890f043d7462767c9172540086f8
Joseph Kerkhof <musicaljoeker@gmail.com>
2018-07-04 19:40:03 -0500

    reorg of navbar again and create sticky footer

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
 import Body from "./components/Body/Body";
-import Nav from "./components/Nav/Nav";
+// import Nav from "./components/Nav/Nav";
 import Footer from "./components/Footer/Footer";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Nav />
+        {/* <Nav /> */}
         <Body />
         <Footer />
       </div>
