7c92cfa73d8093160a962dbc794853d5a881e375
Joseph Kerkhof <musicaljoeker@gmail.com>
2018-07-05 18:40:10 -0500

    basic routing set up

--- src/components/Body/Content/Content.js
+++ src/components/Body/Content/Content.js
@@ -1,10 +1,19 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./Content.css";
 import Home from "./Home/Home";
+import Work from "./Work/Work";
 
 class Content extends Component {
   render() {
-    return <Home />;
+    return (
+      <Router>
+        <div>
+          <Route exact path="/" component={Home} />
+          <Route path="/work" component={Work} />
+        </div>
+      </Router>
+    );
   }
 }
 
---
+++ src/components/Body/Content/Work/Work.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "./Work.css";
+
+class Work extends Component {
+  render() {
+    return (
+      <div>
+        <h1>This is work page</h1>
+        <p>This is work text</p>
+      </div>
+    );
+  }
+}
+
+export default Work;
