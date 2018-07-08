e30d63a090a8e265c78f13d1af2b330f6ea76e51
Joseph Kerkhof <musicaljoeker@gmail.com>
2018-07-08 18:00:24 -0500

    menu now slides in and out

--- src/components/MenuSidebar/MenuSidebar.js
+++ src/components/MenuSidebar/MenuSidebar.js
@@ -5,34 +5,38 @@ import { Link } from "react-router-dom";
 
 class MenuSidebar extends Component {
   render() {
-    if (this.props.show) {
-      return (
-        <div className="menu-sidebar-wrapper">
-          <CloseIcon menuToggle={this.props.menuToggle} />
-          <nav>
-            <ul>
-              <li>
-                <Link to="/" onClick={this.props.menuToggle}>
-                  Home
-                </Link>
-              </li>
-              <li>
-                <Link to="/work" onClick={this.props.menuToggle}>
-                  Work
-                </Link>
-              </li>
-              <li>
-                <Link to="/contact" onClick={this.props.menuToggle}>
-                  Contact
-                </Link>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      );
-    } else {
-      return null;
-    }
+    // if (this.props.show) {
+    return (
+      <div
+        className={
+          "menu-sidebar-wrapper " + (this.props.show ? "active" : "disabled")
+        }
+      >
+        <CloseIcon menuToggle={this.props.menuToggle} />
+        <nav>
+          <ul>
+            <li>
+              <Link to="/" onClick={this.props.menuToggle}>
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/work" onClick={this.props.menuToggle}>
+                Work
+              </Link>
+            </li>
+            <li>
+              <Link to="/contact" onClick={this.props.menuToggle}>
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    );
+    // } else {
+    //   return null;
+    // }
   }
 }
 
