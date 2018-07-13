e92bfdadbfa28ea076f6dd71aaf3ecdf23c0c05c
Joseph Kerkhof <musicaljoeker@gmail.com>
2018-07-13 12:58:23 -0500

    Adding content to contact page

--- src/components/Body/Content/Contact/Contact.js
+++ src/components/Body/Content/Contact/Contact.js
@@ -5,8 +5,50 @@ class Contact extends Component {
   render() {
     return (
       <div className="contact-page-wrapper">
-        <h1>This is contact title</h1>
-        <p>This is contact body</p>
+        {" "}
+        <h1>Contact</h1>
+        <div>
+          I am available for contact on a number of platforms. Listed in order
+          of preference.
+          <ul>
+            <li>
+              <a
+                href="mailto:joseph.kerkhof@gmail.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Email
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/josephkerkhof"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                LinkedIn
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://github.com/musicaljoeker"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                GitHub
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://twitter.com/musicaljoeker"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Twitter
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     );
   }
