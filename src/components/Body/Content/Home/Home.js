7f241f235186651fd5d49cdf8ae76aeebe8d226a
Joseph Kerkhof <musicaljoeker@gmail.com>
2018-07-12 20:23:13 -0500

    Home page content proofing

--- src/components/Body/Content/Home/Home.js
+++ src/components/Body/Content/Home/Home.js
@@ -19,19 +19,20 @@ class Home extends Component {
           className="profile-image"
         />
         <p>
-          Nice to meet you! I'm a full stack web developer with a passion for
-          developing great products. With an eye for detail, I've proven the
+          Nice to meet you! I'm a full stack web developer with a motivation to
+          develop great products. With an eye for detail, I've proven the
           ability to write quality code that works at scale. I have a passion
-          for innovating creative solutions to difficult tasks. I currently
-          spend my free time learning new technologies to apply to my
-          profession, and traveling with my lovely wife.
+          for innovating creative solutions to difficult tasks. I spend my free
+          time learning new technologies to apply to my profession, and
+          traveling with my lovely wife.
         </p>
         <p>
           Currently, I’m a Web Application Developer from the University of
-          Wisconsin – Oshkosh. I earned my Undergraduate degree from University
-          of Wisconsin – Oshkosh in Computer Science in 2016 and currently
-          pursuing a Master’s degree in Data Science from University of
-          Wisconsin - Extension.
+          Wisconsin – Oshkosh. I earned my Undergraduate degree from{" "}
+          <abbr title="University of Wisconsin – Oshkosh">UW-Oshkosh</abbr> in
+          Computer Science in 2016. Now I'm pursuing a Master’s degree in Data
+          Science from{" "}
+          <abbr title="University of Wisconsin - Extension">UW-Extension</abbr>.
         </p>
         <p>
           <a
