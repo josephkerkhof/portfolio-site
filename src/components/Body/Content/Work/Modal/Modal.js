8f30eeb929f756c090e4f56d1c6d0ff0b4bc1df9
Joseph Kerkhof <musicaljoeker@gmail.com>
2018-07-19 13:04:32 -0500

    modal now zooms in and out

--- src/components/Body/Content/Work/Modal/Modal.js
+++ src/components/Body/Content/Work/Modal/Modal.js
@@ -3,6 +3,23 @@ import "./Modal.css";
 import Overlay from "../../../../Overlay/Overlay";
 
 class Modal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      zoomedIn: false
+    };
+    this.zoomToggle = this.zoomToggle.bind(this);
+  }
+
+  zoomToggle(e) {
+    e.stopPropagation();
+    if (this.state.zoomedIn) {
+      this.setState({ zoomedIn: false });
+    } else {
+      this.setState({ zoomedIn: true });
+    }
+  }
+
   render() {
     if (this.props.show) {
       return (
@@ -11,7 +28,10 @@ class Modal extends Component {
             <img
               src={this.props.imgSrc}
               alt={this.props.imgAlt}
-              className="modal-img"
+              className={
+                "modal-img " + (this.state.zoomedIn ? "zoomedIn" : "zoomedOut")
+              }
+              onClick={this.zoomToggle}
             />
           </div>
           <Overlay show={this.props.show} menuToggle={this.props.modalToggle} />
